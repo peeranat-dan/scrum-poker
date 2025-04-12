b089dc9b98c5099ac3ca0e5cffe4c2e156d3eb62
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-12 22:41:25 +0700

    chore: add tanstack query eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,9 +3,11 @@ import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import tseslint from "typescript-eslint";
+import pluginQuery from "@tanstack/eslint-plugin-query";
 
 export default tseslint.config(
   { ignores: ["dist"] },
+  ...pluginQuery.configs["flat/recommended"],
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
     files: ["**/*.{ts,tsx}"],
