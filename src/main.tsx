7f63f4ee9312c10fddb527e2c899e6cb686e6f32
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-12 22:49:58 +0700

    feat: initial setup for tanstack query

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router";
-import "./index.css";
 import App from "./App";
+import "./index.css";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -13,6 +17,9 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </StrictMode>
 );
