5d359cb10d11ef5d546fd0b6eae3b5a2248725a5
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-13 16:30:17 +0700

    refactor: remove createdBy field from Session types

--- src/types/session.types.ts
+++ src/types/session.types.ts
@@ -5,14 +5,12 @@ type VotingSystem = "fibonacci" | "t-shirt";
 export interface Session {
   id: string;
   createdAt: Date;
-  createdBy: string;
   name: string;
   votingSystem: VotingSystem;
 }
 
 export interface SessionDoc {
   createdAt: Timestamp;
-  createdBy: string;
   name: string;
   votingSystem: VotingSystem;
 }
