7e5acef20f9b26f24d7dc3799fe653e7b09ed575
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-13 22:22:17 +0700

    feat: add createSession data layer function

---
+++ src/data/session/create-session.ts
@@ -0,0 +1,14 @@
+import { type CreateSessionInput } from "@/types/schema.types";
+import { addDoc, Timestamp } from "firebase/firestore";
+
+import { sessionsCollection } from "../firestore";
+
+export async function createSession(input: CreateSessionInput) {
+  const session = await addDoc(sessionsCollection, {
+    createdAt: Timestamp.now(),
+    name: input.name,
+    votingSystem: input.votingSystem,
+  });
+
+  return session;
+}
