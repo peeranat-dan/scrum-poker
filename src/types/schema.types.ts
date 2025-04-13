f591919ed96a4e2c23983ab0ab999fa8688f9e46
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-13 21:21:59 +0700

    feat: add validation for session name

--- src/types/schema.types.ts
+++ src/types/schema.types.ts
@@ -1,7 +1,11 @@
 import { z } from "zod";
 
 export const CreateSessionSchema = z.object({
-  name: z.string().min(1, "Session name is required"),
+  name: z
+    .string()
+    .trim()
+    .min(1, "Session name is required")
+    .max(50, "Session name is too long"),
   votingSystem: z.enum(["fibonacci", "t-shirt"]),
 });
 
