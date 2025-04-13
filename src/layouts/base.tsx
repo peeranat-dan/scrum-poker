a2b5bbf1f4e617fba221e5722b7636738d96f6ba
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-13 15:25:07 +0700

    feat: add base layout

---
+++ src/layouts/base.tsx
@@ -0,0 +1,18 @@
+import { type PropsWithChildren } from "react";
+import { Outlet } from "react-router";
+
+type BaseLayoutProps = PropsWithChildren;
+
+export default function BaseLayout({ children }: Readonly<BaseLayoutProps>) {
+  return (
+    <div className="flex h-dvh w-screen flex-col">
+      {/* <Header /> */}
+      <div className="flex flex-1 overflow-hidden">
+        {/* <Sidebar /> */}
+        <main className="flex flex-1 overflow-y-auto">
+          {children ?? <Outlet />}
+        </main>
+      </div>
+    </div>
+  );
+}
