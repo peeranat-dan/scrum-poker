08c770a5f171ea9df5aa66b244efdb5d288afa22
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-13 21:01:21 +0700

    style: add w-full in home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ import { toast } from "sonner";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-svh">
+    <div className="flex flex-col items-center justify-center min-h-svh w-full">
       <Button onClick={() => toast("Hello World")}>Click me</Button>
     </div>
   );
