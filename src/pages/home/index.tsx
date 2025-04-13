da7dcc5efea8232d82ab434aae6a63c0c925e6de
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-13 20:49:54 +0700

    refactor: change to add toast instead of log

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/ui/button";
+import { toast } from "sonner";
 
 export default function HomePage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-svh">
-      <Button onClick={() => console.log("aaa")}>Click me</Button>
+      <Button onClick={() => toast("Hello World")}>Click me</Button>
     </div>
   );
 }
