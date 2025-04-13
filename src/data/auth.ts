db68a4fc12b5e269532964e7603d54a5729865f3
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-13 11:36:56 +0700

    feat: setup anonymous authentication

---
+++ src/data/auth.ts
@@ -0,0 +1,13 @@
+import {
+  signInAnonymously as firebaseSignInAnonymously,
+  getAuth,
+} from "firebase/auth";
+import { app } from "./firebase";
+
+const auth = getAuth(app);
+
+async function signInAnonymously() {
+  return await firebaseSignInAnonymously(auth);
+}
+
+export { signInAnonymously };
