1ff2ee2742c9eb4cecb9c67124bd7c667b1988b1
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 10:06:36 +0700

    feat(participant): add firestore converter for participant

---
+++ src/data/participant/firestore-converter.ts
@@ -0,0 +1,24 @@
+import {
+  type Participant,
+  type ParticipantDoc,
+} from "@/types/participant.types";
+import { type DocumentData, type DocumentSnapshot } from "firebase/firestore";
+
+export function participantConverter(
+  doc: DocumentSnapshot<ParticipantDoc, DocumentData>
+): Participant {
+  const data = doc.data();
+
+  if (!data) {
+    throw new Error("Participant not found");
+  }
+
+  return {
+    id: doc.id,
+    displayName: data.displayName,
+    isOwner: data.isOwner,
+    joinedAt: data.joinedAt.toDate(),
+    sessionId: data.sessionId,
+    uid: data.uid,
+  };
+}
