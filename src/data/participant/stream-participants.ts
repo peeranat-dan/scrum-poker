4b8c829f44494f818cb8c2b7fa5cb7a653b96cb3
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 20:28:09 +0700

    feat(participant): stream participants in a session

---
+++ src/data/participant/stream-participants.ts
@@ -0,0 +1,25 @@
+import { type Participant } from "@/types/participant.types";
+import { onSnapshot, query, where } from "firebase/firestore";
+import { participantsCollection } from "../firestore";
+import { participantConverter } from "./firestore-converter";
+
+export function streamParticipants(
+  sessionId: string,
+  callback: (participants: Participant[]) => void
+) {
+  const q = query(participantsCollection, where("sessionId", "==", sessionId));
+  const unsubscribe = onSnapshot(q, (querySnapshot) => {
+    const participants: Participant[] = [];
+
+    // NOTE: forEach here is from querySnapshot, not from the forEach function in array
+    // No need to convert to for const
+    querySnapshot.forEach((doc) => {
+      const participant = participantConverter(doc);
+      participants.push(participant);
+    });
+
+    callback(participants);
+  });
+
+  return unsubscribe;
+}
