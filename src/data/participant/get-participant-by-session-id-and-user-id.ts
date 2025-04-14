b06c31a891c42dde267fdc72fc2cd760655318b4
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 10:08:29 +0700

    feat(participant): add get participant by session id and user id

---
+++ src/data/participant/get-participant-by-session-id-and-user-id.ts
@@ -0,0 +1,26 @@
+import { getDocs, limit, query, where } from "firebase/firestore";
+import { participantsCollection } from "../firestore";
+import { participantConverter } from "./firestore-converter";
+
+export async function getParticipantBySessionIdAndUserId(
+  sessionId: string,
+  userId: string
+) {
+  const participantDoc = await getDocs(
+    query(
+      participantsCollection,
+      where("sessionId", "==", sessionId),
+      where("uid", "==", userId),
+      limit(1)
+    )
+  );
+
+  if (participantDoc.empty) {
+    return undefined;
+  }
+
+  // NOTE: We are assuming that there is only one participant with the same sessionId and userId
+  const data = participantConverter(participantDoc.docs[0]);
+
+  return data;
+}
