7e8d787b430032ab9ea2c85128d5fac7c7b6eec4
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 20:29:13 +0700

    chore(assets): update create game image

--- src/pages/new-game/index.tsx
+++ src/pages/new-game/index.tsx
@@ -18,7 +18,7 @@ export default function NewGamePage() {
             </div>
             <div className="relative hidden bg-muted md:block">
               <img
-                src="/cat-poker.webp"
+                src="/cat-poker-create-game.webp"
                 alt="Cat playing poker"
                 className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.7] dark:grayscale"
               />
