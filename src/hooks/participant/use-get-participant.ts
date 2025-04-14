97e0282d168b3a95e21295e2177011b439d33804
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 13:43:46 +0700

    feat(participant): add options parameter to useGetParticipant hook

--- src/hooks/participant/use-get-participant.ts
+++ src/hooks/participant/use-get-participant.ts
@@ -1,10 +1,16 @@
 import { getParticipantBySessionIdAndUserId } from "@/data/participant/get-participant-by-session-id-and-user-id";
-import { useQuery } from "@tanstack/react-query";
+import { type Participant } from "@/types/participant.types";
+import { type UseMutationOptions, useQuery } from "@tanstack/react-query";
 
-export function useGetParticipant(sessionId: string, userId: string) {
+export function useGetParticipant(
+  sessionId: string,
+  userId: string,
+  options?: UseMutationOptions<Participant | null>
+) {
   return useQuery({
     queryKey: ["participant", sessionId, userId],
     queryFn: () => getParticipantBySessionIdAndUserId(sessionId, userId),
     enabled: !!sessionId && !!userId,
+    ...options,
   });
 }
