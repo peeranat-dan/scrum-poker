02ec21a491e35b3a2827d651106280a954e8cce4
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 22:49:44 +0700

    feat: add finishRound function

---
+++ src/data/round/finish-round.ts
@@ -0,0 +1,23 @@
+import { getVotesByRoundId } from "../vote/get-votes-by-round-id";
+import { updateRound } from "./update-round";
+
+export async function finishRound(roundId: string) {
+  const votes = await getVotesByRoundId(roundId);
+
+  // Filter out votes that have value of -1 and -2
+  const filteredVotes = votes.filter(
+    (vote) => vote.value !== -1 && vote.value !== -2
+  );
+
+  const averageVote = filteredVotes.length
+    ? filteredVotes.reduce((acc, vote) => acc + vote.value, 0) /
+      filteredVotes.length
+    : null;
+
+  await updateRound({
+    id: roundId,
+    status: "revealed",
+    averageVote,
+    revealedAt: new Date(),
+  });
+}
