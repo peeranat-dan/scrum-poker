f781f16ca3298b074c5907a03e4d3612c891a109
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 22:49:08 +0700

    feat(round): add averageVote to Round and UpdateRoundInput types

--- src/types/round.types.ts
+++ src/types/round.types.ts
@@ -7,10 +7,19 @@ export interface Round {
   sessionId: string;
   status: RoundStatus;
   revealedAt: Date | null;
+  averageVote: number | null;
 }
 
 export interface RoundDoc {
   sessionId: string;
   status: RoundStatus;
   revealedAt: Timestamp | null;
+  averageVote: number | null;
+}
+
+export interface UpdateRoundInput {
+  id: string;
+  status?: RoundStatus;
+  revealedAt?: Date | null;
+  averageVote?: number | null;
 }
