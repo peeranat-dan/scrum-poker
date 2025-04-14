8f0f157ba0387e8756007815cc407b5b7a00188f
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 08:10:29 +0700

    feat(session): add firestore converter for session document

---
+++ src/data/session/firestore-converter.ts
@@ -0,0 +1,19 @@
+import { type Session, type SessionDoc } from "@/types/session.types";
+import { type DocumentData, type DocumentSnapshot } from "firebase/firestore";
+
+export function sessionConverter(
+  doc: DocumentSnapshot<SessionDoc, DocumentData>
+): Session {
+  const data = doc.data();
+
+  if (!data) {
+    throw new Error("Session not found");
+  }
+
+  return {
+    id: doc.id,
+    createdAt: data.createdAt.toDate(),
+    name: data.name,
+    votingSystem: data.votingSystem,
+  };
+}
