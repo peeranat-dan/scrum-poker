a1ba8cd664091d7eb56e8c1fb00f335d69aeaf7e
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 13:44:10 +0700

    feat(game): simplify GameProvider and fetch participants

--- src/providers/game/game-provider.tsx
+++ src/providers/game/game-provider.tsx
@@ -1,65 +1,27 @@
-import { useGetParticipant } from "@/hooks/participant/use-get-participant";
-import { useUpdateParticipantName } from "@/hooks/participant/use-update-participant-name";
-import { useGetSessionById } from "@/hooks/session/use-get-session-by-id";
+import { useGetParticipantsBySessionId } from "@/hooks/participant/use-get-participants-by-session-id";
 import { getCards } from "@/lib/card";
-import { useCallback, useMemo } from "react";
-import { useAuth } from "../auth";
+import { useMemo } from "react";
+import { useSession } from "../session";
 import { GameContext } from "./game-context";
 import { type GameProviderProps } from "./types";
-import { useQueryClient } from "@tanstack/react-query";
 
-export function GameProvider({
-  gameId,
-  children,
-}: Readonly<GameProviderProps>) {
-  const queryClient = useQueryClient();
-  const { data, isLoading, error } = useGetSessionById(gameId);
-  const { user } = useAuth();
-  const { data: participantData, isLoading: isParticipantLoading } =
-    useGetParticipant(gameId, user?.uid ?? "");
+export function GameProvider({ children }: Readonly<GameProviderProps>) {
+  const session = useSession();
 
-  const updateParticipantNameMutation = useUpdateParticipantName();
-
-  const updatePlayerName = useCallback(
-    async (name: string) => {
-      if (!participantData) return;
-      await updateParticipantNameMutation.mutateAsync(
-        {
-          participantId: participantData.id,
-          name,
-        },
-        {
-          onSuccess: () => {
-            queryClient.invalidateQueries({
-              queryKey: ["participant", data?.id, user?.uid],
-            });
-          },
-        }
-      );
-    },
-    [
-      participantData,
-      updateParticipantNameMutation,
-      data?.id,
-      user?.uid,
-      queryClient,
-    ]
-  );
+  const { data: participantsData, isLoading: isParticipantsLoading } =
+    useGetParticipantsBySessionId(session.id);
 
   const value = useMemo(
     () => ({
-      session: data,
-      loading: isLoading || isParticipantLoading,
-      cards: data?.votingSystem ? getCards(data.votingSystem) : [],
-      playerInfo: participantData,
-      updatePlayerName,
+      session,
+      cards: getCards(session.votingSystem),
+      participants: participantsData ?? [],
     }),
-    [data, isLoading, isParticipantLoading, participantData, updatePlayerName]
+    [session, participantsData]
   );
 
-  if (error) {
-    // TODO: Handle error
-    throw error;
+  if (isParticipantsLoading) {
+    return <div>Loading...</div>;
   }
 
   return <GameContext.Provider value={value}>{children}</GameContext.Provider>;
