aba42bf4edbfc498c68b20be37c9bab79b3977de
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 22:17:28 +0700

    feat: display participant votes with status handling

--- src/containers/game-participants.tsx
+++ src/containers/game-participants.tsx
@@ -1,11 +1,24 @@
 import { cn } from "@/lib/utils";
 import { useGame } from "@/providers/game";
+import { type RoundStatus } from "@/types/round.types";
+
+function getVoteValue(voteValue?: number, roundStatus?: RoundStatus) {
+  if (!roundStatus) {
+    return "🤷🏻‍♂️";
+  }
+
+  if (roundStatus === "revealed") {
+    return voteValue;
+  }
+
+  return voteValue ? "👍🏼" : "🤔";
+}
 
 export default function GameParticipants() {
-  const { participants } = useGame();
+  const { participants, round } = useGame();
 
   return (
-    <div className="flex gap-2">
+    <div className="flex gap-4">
       {participants.map((participant) => (
         <div key={participant.id} className="flex flex-col items-center gap-2">
           <div className="text-center text-sm font-semibold">
@@ -13,12 +26,11 @@ export default function GameParticipants() {
           </div>
           <div
             className={cn(
-              "flex w-20 aspect-[2/3] shrink-0 items-center justify-center rounded-md border border-accent bg-background text-center text-2xl font-semibold font-mono text-foreground shadow-md"
-              //   voteValue && "bg-primary text-primary-foreground hover:bg-primary/70"
+              "flex w-24 aspect-[2/3] shrink-0 items-center rounded-md justify-center border border-accent bg-background text-center text-2xl font-semibold font-mono text-foreground shadow-md",
+              !participant.vote && "bg-accent text-accent-foreground"
             )}
           >
-            {/* TODO: Handle vote value here {voteValue ?? "🤔"} */}
-            😇
+            {getVoteValue(participant.vote, round?.status)}
           </div>
         </div>
       ))}
