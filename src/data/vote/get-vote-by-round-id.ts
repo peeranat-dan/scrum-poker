9589160251b8a1e20957529dd603cbf6424b390e
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-14 14:40:46 +0700

    feat(vote): add getVoteByRoundId function

---
+++ src/data/vote/get-vote-by-round-id.ts
@@ -0,0 +1,20 @@
+import { getDocs, limit, query, where } from "firebase/firestore";
+import { votesCollection } from "../firestore";
+import { voteConverter } from "./firestore-converter";
+
+export async function getVoteByRoundId(roundId: string, participantId: string) {
+  const voteSnapshot = await getDocs(
+    query(
+      votesCollection,
+      where("roundId", "==", roundId),
+      where("participantId", "==", participantId),
+      limit(1)
+    )
+  );
+
+  if (voteSnapshot.empty) {
+    return undefined;
+  }
+
+  return voteConverter(voteSnapshot.docs[0]);
+}
