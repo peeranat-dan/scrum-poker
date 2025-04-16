a89c4d632c1dc39a7d8f8707195572f957343ec8
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-16 21:45:30 +0700

    feat(game): integrate SessionInformationContainer into GameSettingsGeneralPage

--- src/pages/game/[gameId]/settings/general/index.tsx
+++ src/pages/game/[gameId]/settings/general/index.tsx
@@ -1,10 +1,18 @@
+import { Card, CardContent } from "@/components/ui/card";
+import { Separator } from "@/components/ui/separator";
+import SessionInformationContainer from "@/containers/session-information-container";
 import SessionTerminationButton from "@/containers/session-termination-button";
 
 export default function GameSettingsGeneralPage() {
   return (
     <div className="flex flex-col w-full gap-8 relative px-4 max-w-5xl mx-auto">
       <h2>General Settings</h2>
-      {/* TODO: Implement game name update here */}
+      <Card>
+        <CardContent className="space-y-4">
+          <SessionInformationContainer />
+        </CardContent>
+      </Card>
+      <Separator />
       <section className="flex flex-col gap-4">
         <h3>Danger Zone</h3>
         <SessionTerminationButton />
