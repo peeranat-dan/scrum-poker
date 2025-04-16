be8cb83af1554148bb95a60e6d09e9dd7358370c
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-16 18:06:34 +0700

    feat(participant): ensure user is logged in before leaving session

--- src/hooks/participant/use-leave-session.ts
+++ src/hooks/participant/use-leave-session.ts
@@ -1,7 +1,14 @@
 import { leaveSession } from "@/data/participant/leave-session";
+import { useAuth } from "@/providers/auth";
 import { useMutation } from "@tanstack/react-query";
 
 export function useLeaveSession() {
+  const { user } = useAuth();
+
+  if (!user) {
+    throw new Error("Not logged in");
+  }
+
   return useMutation({
     mutationFn: leaveSession,
   });
