086cbbbb92b255ac0f2479a5a22ac15e8f3e0c92
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-17 20:04:59 +0700

    refactor: move vote calculation to VoteCharts container

--- src/containers/game-area.tsx
+++ src/containers/game-area.tsx
@@ -1,30 +1,10 @@
-import VotesChart from "@/components/votes-chart";
 import { useGame } from "@/providers/game";
 import GameCards from "./game-cards";
 import GameAverage from "./game-average";
+import VotesChart from "./vote-charts";
 
 export default function GameArea() {
-  const { round, participants, cards } = useGame();
-
-  const voteCounts = participants.reduce((acc, participant) => {
-    const vote = participant.vote;
-    if (vote) {
-      acc[vote] = (acc[vote] || 0) + 1;
-    }
-    return acc;
-  }, {} as Record<number, number>);
-
-  const votes = Object.entries(voteCounts)
-    .map(([vote, count]) => {
-      return {
-        label:
-          cards.find((card) => card.value === parseInt(vote))?.displayValue ??
-          "",
-        vote: vote,
-        count,
-      };
-    })
-    .sort((a, b) => parseInt(a.vote) - parseInt(b.vote));
+  const { round } = useGame();
 
   if (!round) {
     return null;
@@ -36,7 +16,7 @@ export default function GameArea() {
 
   return (
     <div className="flex items-center gap-4 w-full justify-center">
-      <VotesChart votes={votes} />
+      <VotesChart />
       <GameAverage />
     </div>
   );
