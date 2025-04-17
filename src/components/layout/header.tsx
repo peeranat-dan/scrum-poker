64c97d326357516bd8969e16b1b3d3a8fa86e6ef
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-17 10:46:41 +0700

    feat: add Header component

---
+++ src/components/layout/header.tsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router";
+import { buttonVariants } from "../ui/button-variants";
+import { ThemeToggle } from "../theme-toggle";
+
+export default function Header() {
+  return (
+    <header className="w-full border-b px-6 md:px-8 bg-background/80 backdrop-blur">
+      <div className="container flex h-[3.5rem] items-center justify-between mx-auto">
+        <Link to="/" className="flex items-center gap-2">
+          <div className="text-2xl font-mono font-semibold">E-mate</div>
+        </Link>
+        <div className="ml-auto flex h-full items-center gap-2">
+          <ThemeToggle />
+          <Link
+            className={buttonVariants({ variant: "secondary" })}
+            to="/new-game"
+          >
+            Start new game
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
