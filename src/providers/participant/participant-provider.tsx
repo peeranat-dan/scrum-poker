be191551f63345469a0ede2e962b7df94a8c466b
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-20 09:40:21 +0700

    fix: update participant id key in updateParticipantNameMutation

--- src/providers/participant/participant-provider.tsx
+++ src/providers/participant/participant-provider.tsx
@@ -25,7 +25,7 @@ export function ParticipantProvider({
       if (!participant) return;
       await updateParticipantNameMutation.mutateAsync(
         {
-          participantId: participant.id,
+          id: participant.id,
           name,
         },
         {
