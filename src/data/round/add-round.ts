796c625cf2d3881ebaab5fe58e5a305a33688891
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-26 20:14:55 +0700

    fix(round): add createdAt and updatedAt when adding new round

--- src/data/round/add-round.ts
+++ src/data/round/add-round.ts
@@ -1,8 +1,12 @@
-import { addDoc } from "firebase/firestore";
+import { addDoc, serverTimestamp } from "firebase/firestore";
 import { roundsCollection } from "../firestore";
 import { type AddRoundInput } from "./types";
 
 export async function addRound(input: AddRoundInput) {
-  const round = await addDoc(roundsCollection, input);
+  const round = await addDoc(roundsCollection, {
+    ...input,
+    createdAt: serverTimestamp(),
+    updatedAt: serverTimestamp(),
+  });
   return round;
 }
