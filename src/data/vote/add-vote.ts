af0abbe3206ecc435069afdcd12ec2f5125f87eb
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-04-26 20:15:03 +0700

    fix(vote): add createdAt and updatedAt when adding new vote

--- src/data/vote/add-vote.ts
+++ src/data/vote/add-vote.ts
@@ -1,9 +1,13 @@
-import { addDoc } from "firebase/firestore";
+import { addDoc, serverTimestamp } from "firebase/firestore";
 
 import { votesCollection } from "../firestore";
 import { type AddVoteInput } from "./types";
 
 export async function addVote(input: AddVoteInput) {
-  const vote = await addDoc(votesCollection, input);
+  const vote = await addDoc(votesCollection, {
+    ...input,
+    createdAt: serverTimestamp(),
+    updatedAt: serverTimestamp(),
+  });
   return vote;
 }
