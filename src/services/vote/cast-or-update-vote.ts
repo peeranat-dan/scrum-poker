b6f50535ef6a375badae5aabfccacb2721637f75
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-05-07 15:41:24 +0700

    fix(vote): assert valid cast vote schema

--- src/services/vote/cast-or-update-vote.ts
+++ src/services/vote/cast-or-update-vote.ts
@@ -1,14 +1,14 @@
 import { getRound } from '@/data/round/get-round';
 import { findVote } from '@/data/vote/find-vote';
 import { canCastOrUpdateVote } from '@/domain/vote/rules';
-
+import { assertValid } from '@/shared/zod/utils';
 import { castVote } from './cast-vote';
+import { CastVoteSchema } from './schemas';
 import { type CastVoteInput } from './types';
 import { updateVoteValue } from './update-vote-value';
 
 export async function castOrUpdateVote(input: CastVoteInput) {
-  // NOTE: We do not validate the input here because it is validated in the castVote and updateVoteValue functions
-  const { participantId, roundId, value } = input;
+  const { participantId, roundId, value } = assertValid(CastVoteSchema, input);
 
   const existingVote = await findVote({
     filter: {
