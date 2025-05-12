773cdb35eb3a697c30ffd1a5db029771745d3d52
Peeranat Danaidusadeekul <85337926+peeranat-dan@users.noreply.github.com>
2025-05-12 19:01:56 +0700

    feat: add new home page sections (#140)
    * refactor: change layout to apply at layout instead of index file
    
    * feat(home): add new home page section

--- src/layouts/base.tsx
+++ src/layouts/base.tsx
@@ -4,9 +4,9 @@ import { Outlet } from 'react-router';
 
 export default function BaseLayout() {
   return (
-    <div className='relative flex min-h-dvh w-screen flex-col'>
+    <div className='relative flex min-h-dvh w-full flex-col'>
       <Header />
-      <main className='flex flex-1'>
+      <main className='flex-1'>
         <Outlet />
       </main>
       <Toaster />
