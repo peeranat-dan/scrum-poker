db41eebbc74d29a1301adf2742ce7a513c855731
Apisit Likitwattanapaisarn <aphisit.hiang@skooldio.com>
2025-05-19 00:51:58 +0700

    refactor(profile): rename UserProfileModal to EditUserProfileButton and adding trigger button

--- src/containers/edit-user-profile-button.tsx
+++ src/containers/edit-user-profile-button.tsx
@@ -5,19 +5,19 @@ import {
   DialogDescription,
   DialogHeader,
   DialogTitle,
+  DialogTrigger,
 } from '@/components/ui/dialog';
 import config from '@/config';
 import { useParticipant } from '@/providers/participant';
 import { type UserProfileInput, UserProfileSchema } from '@/types/schema.types';
 import { zodResolver } from '@hookform/resolvers/zod';
+import { Edit } from 'lucide-react';
+import { useEffect, useState } from 'react';
 import { useForm } from 'react-hook-form';
+import { Button } from '../components/ui/button';
 
-interface UserProfileModalProps {
-  isOpen?: boolean;
-  setIsOpen?: (isOpen: boolean) => void;
-}
-
-export default function UserProfileModal({ isOpen, setIsOpen }: UserProfileModalProps = {}) {
+export default function EditUserProfileButton() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   const { participant, updateParticipantName } = useParticipant();
   const form = useForm<UserProfileInput>({
     resolver: zodResolver(UserProfileSchema),
@@ -26,13 +26,24 @@ export default function UserProfileModal({ isOpen, setIsOpen }: UserProfileModal
     },
   });
 
+  useEffect(() => {
+    if (participant?.displayName === config.game.defaultParticipantName) {
+      setIsModalOpen(true);
+    }
+  }, [participant]);
+
   const onSubmit = (data: UserProfileInput) => {
     updateParticipantName(data.displayName);
-    setIsOpen?.(false);
+    setIsModalOpen(false);
   };
 
   return (
-    <Dialog open={isOpen} onOpenChange={setIsOpen}>
+    <Dialog open={isModalOpen} onOpenChange={setIsModalOpen}>
+      <DialogTrigger asChild>
+        <Button variant='ghost' size='icon'>
+          <Edit />
+        </Button>
+      </DialogTrigger>
       <DialogContent>
         <DialogHeader>
           <DialogTitle>Edit your profile</DialogTitle>
