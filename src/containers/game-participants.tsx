cb7e7fd738a0a3c76975f5152d1df3a71acd473b
Aphisit Likitwattanapaisarn <Aphisit.heung@gmail.com>
2025-05-19 21:19:18 +0700

    feat(participant): allow participant to edit their name (#152)
    * feat(participant): allow participant to edit their name
    
    * refactor(participant): remove unused edit name button component
    
    * refactor(profile): rename UserProfileModal to EditUserProfileButton and adding trigger button
    
    * refactor(participant): change ParticipantEditNameButton to EditUserProfileButton in GameParticipants
    
    * chore(user-profile): update alias import
    
    Co-authored-by: Peeranat Danaidusadeekul <85337926+peeranat-dan@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Apisit Likitwattanapaisarn <aphisit.hiang@skooldio.com>
    Co-authored-by: Peeranat Danaidusadeekul <85337926+peeranat-dan@users.noreply.github.com>

--- src/containers/game-participants.tsx
+++ src/containers/game-participants.tsx
@@ -2,6 +2,8 @@ import { type RoundStatus } from '@/data/round/types';
 import { cn } from '@/lib/cn';
 import { useGame } from '@/providers/game';
 import { type Card } from '@/shared/card/types';
+import { useParticipant } from '../providers/participant';
+import EditUserProfileButton from './edit-user-profile-button';
 
 function getVoteValue(cards: Card[], voteValue?: number, roundStatus?: RoundStatus) {
   if (!roundStatus) {
@@ -29,6 +31,7 @@ function getCardColor(cards: Card[], voteValue?: number, roundStatus?: RoundStat
 
 export default function GameParticipants() {
   const { participants, round, cards } = useGame();
+  const { participant: currentParticipant } = useParticipant();
 
   return (
     <ul className='no-scrollbar mx-auto my-0 flex w-full gap-2 overflow-x-auto overflow-y-hidden p-4 lg:w-fit'>
@@ -37,16 +40,23 @@ export default function GameParticipants() {
           <div className='max-w-[96px] truncate text-center text-sm font-semibold'>
             {participant.displayName}
           </div>
-          <div
-            className={cn(
-              'bg-card text-foreground flex aspect-[2/3] w-24 shrink-0 items-center justify-center rounded-md border text-center font-mono text-2xl font-semibold shadow-md',
-              'origin-[center_bottom] rotate-y-[360deg] transition-all',
-              getCardColor(cards, participant.vote, round?.status),
-              typeof participant.vote === 'undefined' && 'bg-accent text-accent-foreground',
-              round?.status === 'revealed' && 'rotate-y-0',
+          <div className='relative'>
+            {participant.id === currentParticipant?.id && (
+              <div className='absolute top-0 right-0 z-10'>
+                <EditUserProfileButton />
+              </div>
             )}
-          >
-            {getVoteValue(cards, participant.vote, round?.status)}
+            <div
+              className={cn(
+                'bg-card text-foreground flex aspect-[2/3] w-24 shrink-0 items-center justify-center rounded-md border text-center font-mono text-2xl font-semibold shadow-md',
+                'origin-[center_bottom] rotate-y-[360deg] transition-all',
+                getCardColor(cards, participant.vote, round?.status),
+                typeof participant.vote === 'undefined' && 'bg-accent text-accent-foreground',
+                round?.status === 'revealed' && 'rotate-y-0',
+              )}
+            >
+              {getVoteValue(cards, participant.vote, round?.status)}
+            </div>
           </div>
         </li>
       ))}
