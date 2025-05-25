ca68abe78410ac6176676eb96401358a93add64d
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-05-25 13:37:16 +0700

    feat(game): add delay for reveal round action for 2 seconds

--- src/containers/game-actions.tsx
+++ src/containers/game-actions.tsx
@@ -2,13 +2,25 @@ import { Button } from '@/components/ui/button';
 import { useGame } from '@/providers/game';
 import { useParticipant } from '@/providers/participant';
 import { copyJoinLink } from '@/shared/utils/copy-join-link';
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import { toast } from 'sonner';
 import SessionLeaveButton from './session-leave-button';
 
 export default function GameActions() {
   const { participant } = useParticipant();
   const { round, revealRound, revoteRound, startNewRound, participants } = useGame();
+  const [isCooldown, setIsCooldown] = useState(false);
+
+  const handleRevealRound = () => {
+    revealRound();
+    setIsCooldown(true);
+
+    const timer = setTimeout(() => {
+      setIsCooldown(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  };
 
   const handleCopyJoinLink = () => {
     if (participant?.sessionId) {
@@ -28,12 +40,16 @@ export default function GameActions() {
     return (
       <>
         {round?.status === 'in-progress' ? (
-          <Button onClick={revealRound} disabled={shouldDisableRevealVoteButton} className='w-full'>
+          <Button
+            onClick={handleRevealRound}
+            disabled={shouldDisableRevealVoteButton}
+            className='w-full'
+          >
             Reveal Vote
           </Button>
         ) : (
           <>
-            <Button onClick={startNewRound} className='flex-1'>
+            <Button onClick={startNewRound} disabled={isCooldown} className='flex-1'>
               Start New Round
             </Button>
             <Button onClick={revoteRound} variant='outline'>
