977cd5a2a7955f997d2c8193bd6576b8c728f97b
Peeranat Danaidusadeekul <ppeeranat.d@gmail.com>
2025-06-04 23:14:47 +0700

    fix(vote): use composite key for vote document to prevent race conditions

--- src/data/vote/add-vote.ts
+++ src/data/vote/add-vote.ts
@@ -1,17 +1,19 @@
-import { addDoc, doc, getDoc, serverTimestamp } from 'firebase/firestore';
+import { doc, getDoc, serverTimestamp, setDoc } from 'firebase/firestore';
 
 import { votesCollection } from '../firestore';
 import { voteMapper } from './mapper';
 import { type AddVoteInput } from './types';
 
 export async function addVote(input: AddVoteInput) {
-  const vote = await addDoc(votesCollection, {
+  const id = `${input.roundId}-${input.participantId}`;
+  // NOTE: We use the roundId and participantId as the id to ensure that the vote is unique and to avoid race conditions
+  await setDoc(doc(votesCollection, id), {
     ...input,
     createdAt: serverTimestamp(),
     updatedAt: serverTimestamp(),
   });
 
-  const voteDoc = await getDoc(doc(votesCollection, vote.id));
+  const voteDoc = await getDoc(doc(votesCollection, id));
 
   return voteMapper.toVote(voteDoc);
 }
